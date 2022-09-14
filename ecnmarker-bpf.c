d5ab01ecfadb441bf99e0e0873f19fa47f25a841
Stijn Tintel <stijn@linux-ipv6.be>
2022-09-14 20:47:58 +0300

    ecnmarker: read ethertype from sk_buff
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- ecnmarker-bpf.c
+++ ecnmarker-bpf.c
@@ -5,6 +5,26 @@
 SEC("tc")
 int ecnmarker(struct __sk_buff *skb)
 {
+	struct ethhdr *eth;
+	uint16_t ethertype;
+
+	void *data = (void *)(long)skb->data;
+	void *data_end = (void *)(long)skb->data_end;
+
+	if (data + sizeof(struct ethhdr) > data_end) {
+		bpf_printk("Frame shorter than ethhdr, aborting\n");
+		goto out;
+	}
+
+	eth = data;
+	ethertype = eth->h_proto;
+
+	switch (ethertype) {
+		default:
+			break;
+	}
+
+out:
 	// use default action configured from tc
 	// BTF does not record #define macros so TC_ACT_* are missing from vmlinux.h
 	return -1;	// TC_ACT_UNSPEC
